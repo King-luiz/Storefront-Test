bf24e63422a50005a4d327fda55f3fa117b8c66e
Lewins Mureithi <161879514+King-luiz@users.noreply.github.com>
2026-02-02 01:20:37 -0500

    Add files via upload

---
+++ luizapp/luizapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LuizappConfig(AppConfig):
+    name = 'luizapp'
---
+++ luizapp/luizapp/urls.py
@@ -0,0 +1,10 @@
+#map urls
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+]
+
